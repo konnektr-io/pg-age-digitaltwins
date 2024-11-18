9787e4b1bafe52dc8c96d9a33bffd190a4b5fdf5
Niko Raes <niko.raes@arcadis.com>
2024-11-18 19:27:07 +0100

    Prepare api

--- src/AgeDigitalTwins.ApiService/Program.cs
+++ src/AgeDigitalTwins.ApiService/Program.cs
@@ -1,3 +1,6 @@
+using System.Text.Json;
+using AgeDigitalTwins;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add service defaults & Aspire client integrations.
@@ -9,6 +12,26 @@ builder.Services.AddProblemDetails();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+// Configure and register AgeDigitalTwinsClient
+builder.Services.AddSingleton<AgeDigitalTwinsClient>(serviceProvider =>
+{
+    var configuration = new ConfigurationBuilder()
+        .SetBasePath(AppContext.BaseDirectory)
+        .AddJsonFile("appsettings.Development.json")
+        .Build();
+
+    string connectionString = Environment.GetEnvironmentVariable("AGE_CONNECTION_STRING")
+        ?? configuration.GetConnectionString("AgeConnectionString")
+        ?? throw new ArgumentNullException("AgeConnectionString");
+
+    var graphName = Environment.GetEnvironmentVariable("AGE_CONNECTION_STRING")
+        ?? configuration.GetConnectionString("AgeGraphName")
+        ?? "digitaltwins";
+    var client = new AgeDigitalTwinsClient(connectionString, new() { GraphName = graphName });
+    client.CreateGraphAsync().GetAwaiter().GetResult();
+    return client;
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -19,27 +42,18 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-string[] summaries = ["Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"];
-
-app.MapGet("/digitaltwins/{id}", (string id) =>
+app.MapGet("/digitaltwins/{id}", (string id, AgeDigitalTwinsClient client) =>
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    return client.GetDigitalTwinAsync<JsonDocument>(id);
 })
-.WithName("GetDigitalTwinById");
+.WithName("GetDigitalTwin");
+
+app.MapPut("/digitaltwins/{id}", (string id, JsonDocument digitalTwin, AgeDigitalTwinsClient client) =>
+{
+    return client.CreateOrReplaceDigitalTwinAsync(id, digitalTwin);
+})
+.WithName("CreateOrReplaceDigitalTwin");
 
 app.MapDefaultEndpoints();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
