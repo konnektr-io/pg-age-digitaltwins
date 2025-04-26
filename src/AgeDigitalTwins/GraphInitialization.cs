63275a90a83895d9bbc0b272e3163a017388c7ca
Niko Raes <niko.raes@arcadis.com>
2025-04-26 08:52:48 +0200

    Refactor agtype functions to check for existence before creation and improve error handling

--- src/AgeDigitalTwins/GraphInitialization.cs
+++ src/AgeDigitalTwins/GraphInitialization.cs
@@ -54,65 +54,81 @@ public static class GraphInitialization
                 $function$"
             ),
             new(
-                @$"CREATE OR REPLACE FUNCTION public.agtype_set(target agtype, path agtype, new_value agtype)
-                RETURNS agtype AS $$
-                DECLARE
-                    json_target jsonb;
-                    json_new_value jsonb;
-                    text_path text[];
+                @$"DO $$
                 BEGIN
-                    -- Cast agtype to jsonb
-                    json_target := target::json::jsonb;
+                    IF NOT EXISTS (
+                        SELECT 1 FROM pg_proc WHERE proname = 'agtype_set'
+                    ) THEN
+                        CREATE OR REPLACE FUNCTION public.agtype_set(target agtype, path agtype, new_value agtype)
+                        RETURNS agtype AS $$
+                        DECLARE
+                            json_target jsonb;
+                            json_new_value jsonb;
+                            text_path text[];
+                        BEGIN
+                            -- Cast agtype to jsonb
+                            json_target := target::json::jsonb;
 
-                    -- Convert agtype path to text array
-                    text_path := ARRAY(SELECT json_array_elements_text(path::json));
+                            -- Convert agtype path to text array
+                            text_path := ARRAY(SELECT json_array_elements_text(path::json));
 
-                    BEGIN
-                        -- Attempt to cast new_value to jsonb directly
-                        json_new_value := new_value::json::jsonb;
-                    EXCEPTION
-                        WHEN others THEN
-                            -- Handle different types of new_value
-                            IF new_value::text ~ '^[0-9]+$|^[0-9]*\.[0-9]+$|^[0-9]+(\.[0-9]+)?[eE][+-]?[0-9]+$' THEN
-                                -- Convert all numeric values to float
-                                json_new_value := to_jsonb(new_value::float);
-                            ELSIF new_value::text = 'true' OR new_value::text = 'false' THEN
-                                -- Boolean
-                                json_new_value := to_jsonb(new_value::boolean);
-                            ELSE
-                                -- Default to text
-                                json_new_value := to_jsonb(new_value);
-                            END IF;
-                    END;
+                            BEGIN
+                                -- Attempt to cast new_value to jsonb directly
+                                json_new_value := new_value::json::jsonb;
+                            EXCEPTION
+                                WHEN others THEN
+                                    -- Handle different types of new_value
+                                    IF new_value::text ~ '^[0-9]+$|^[0-9]*\\.[0-9]+$|^[0-9]+(\\.[0-9]+)?[eE][+-]?[0-9]+$' THEN
+                                        -- Convert all numeric values to float
+                                        json_new_value := to_jsonb(new_value::float);
+                                    ELSIF new_value::text = 'true' OR new_value::text = 'false' THEN
+                                        -- Boolean
+                                        json_new_value := to_jsonb(new_value::boolean);
+                                    ELSE
+                                        -- Default to text
+                                        json_new_value := to_jsonb(new_value);
+                                    END IF;
+                            END;
 
-                    -- Use jsonb_set to update the json value
-                    json_target := jsonb_set(json_target::jsonb, text_path, json_new_value);
+                            -- Use jsonb_set to update the json value
+                            json_target := jsonb_set(json_target::jsonb, text_path, json_new_value);
 
-                    -- Cast the result back to agtype
-                    RETURN json_target::text::agtype;
+                            -- Cast the result back to agtype
+                            RETURN json_target::text::agtype;
+                        END;
+                        $$ LANGUAGE plpgsql;
+                    END IF;
                 END;
-                $$ LANGUAGE plpgsql;"
+                $$"
             ),
             new(
-                @$"CREATE OR REPLACE FUNCTION public.agtype_delete_key(target agtype, path agtype)
-                RETURNS agtype AS $$
-                DECLARE
-                    json_target jsonb;
-                    text_path text[];
+                @$"DO $$
                 BEGIN
-                    -- Cast agtype to jsonb
-                    json_target := target::json::jsonb;
+                    IF NOT EXISTS (
+                        SELECT 1 FROM pg_proc WHERE proname = 'agtype_delete_key'
+                    ) THEN
+                        CREATE OR REPLACE FUNCTION public.agtype_delete_key(target agtype, path agtype)
+                        RETURNS agtype AS $$
+                        DECLARE
+                            json_target jsonb;
+                            text_path text[];
+                        BEGIN
+                            -- Cast agtype to jsonb
+                            json_target := target::json::jsonb;
 
-                    -- Convert agtype path to text array
-                    text_path := ARRAY(SELECT json_array_elements_text(path::json));
+                            -- Convert agtype path to text array
+                            text_path := ARRAY(SELECT json_array_elements_text(path::json));
 
-                    -- Use jsonb delete key to update the json value
-                    json_target := json_target  #- text_path;
+                            -- Use jsonb delete key to update the json value
+                            json_target := json_target #- text_path;
 
-                    -- Cast the result back to agtype
-                    RETURN json_target::text::agtype;
+                            -- Cast the result back to agtype
+                            RETURN json_target::text::agtype;
+                        END;
+                        $$ LANGUAGE plpgsql;
+                    END IF;
                 END;
-                $$ LANGUAGE plpgsql;"
+                $$"
             ),
         ];
     }
